3925db2e2d027b885c157ce538a77accd5ac187d
Ashish Sharda <ashishjsharda@gmail.com>
2023-12-23 15:36:13 -0500

    Update Item

--- src/internal/model/item.go
+++ src/internal/model/item.go
@@ -1,1 +1,38 @@
 package model
+
+import "errors"
+
+var items []Item
+
+type Item struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func GetAllItems() []Item {
+	return items
+}
+
+func AddItem(item Item) {
+	items = append(items, item)
+}
+
+func UpdateItem(id string, newItem Item) (Item, error) {
+	for i, item := range items {
+		if item.ID == id {
+			items[i] = newItem
+			return newItem, nil
+		}
+	}
+	return Item{}, errors.New("item not found")
+}
+
+func DeleteItem(id string) error {
+	for i, item := range items {
+		if item.ID == id {
+			items = append(items[:i], items[i+1:]...)
+			return nil
+		}
+	}
+	return errors.New("item not found")
+}
