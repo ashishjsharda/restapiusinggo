1269cf77bad2d6822c40b9c4cd596ac00a71df74
Ashish Sharda <ashishjsharda@gmail.com>
2023-12-23 17:50:50 -0500

    update main

--- src/cmd/server/main.go
+++ src/cmd/server/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"restfulGoCart/src/internal/api"
+)
 
+func main() {
+	r := mux.NewRouter()
+
+	api.SetupRoutes(r)
+
+	// Start server
+	log.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
